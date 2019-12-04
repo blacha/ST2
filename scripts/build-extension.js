6da893b2ef59fe4f6205f34c5f77b1c086b91eb4
Blayne Chard <blayne@chard.com>
2019-12-04 20:27:51 +1300

    feat: add git hash + version to built package

--- scripts/build-extension.js
+++ scripts/build-extension.js
@@ -1,15 +1,19 @@
 const ncc = require('@zeit/ncc');
 const fs = require('fs');
+const gitRev = require('git-rev-sync');
+const packageJson = require('../package.json');
 
 async function main() {
     const output = await ncc('./build/src/extension/index.js');
     const lines = output.code.split('\n');
     lines.shift();
-    // lines.pop();
-    // lines.push(`/******/ })();`);
+    const code = lines
+        .join('\n')
+        .replace(' __dirname + ', '')
+        .replace('__VERSION__', packageJson.version)
+        .replace('__HASH__', gitRev.long());
 
-    const code = lines.join('\n').replace(' __dirname + ', '');
-    fs.writeFileSync('./extension.js', code);
+    fs.writeFileSync('./dist/extension.js', code);
 }
 
 main().catch(e => console.log(e));
