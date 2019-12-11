d6ff8464b38bef775a9ba498b4882eca51d3748a
Blayne Chard <blayne@chard.com>
2019-12-11 19:55:45 +1300

    feat: simple css

--- src/web/app.tsx
+++ src/web/app.tsx
@@ -5,9 +5,23 @@ import { ViewLandingPage } from './landing/landing';
 import { ViewScan } from './scan/scan.result'
 import { style } from 'typestyle';
 
+
 const AppCss = {
     Main: style({
-        fontFamily: '"Roboto Condensed"'
+        fontFamily: '"Roboto Condensed"',
+        color: 'rgba(0,0,0,0.87)'
+    }),
+    Header: style({
+        background: '#f4f5f6',
+        borderBottom: '.1rem solid #d1d1d1',
+        display: 'block',
+        height: '5.2rem',
+        left: 0,
+        maxWidth: '100%',
+        position: 'fixed',
+        right: 0,
+        top: 0,
+        width: '100%',
     })
 }
 
@@ -16,13 +30,21 @@ export class App extends React.Component {
         return (
             <BrowserRouter>
                 <div className={AppCss.Main}>
-                    <Switch>
-                        <Route exact={true} path="/" component={ViewLandingPage} />
-                        <Route path="/base/:baseId" component={ViewBase} />
-                        <Route path="/base" component={ViewBase} />
-                        <Route path="/scan/:scanId" component={ViewScan} />
-                        <Redirect to="/" />
-                    </Switch>
+                    <div className={AppCss.Header}>
+                        <section className="container">
+                            <div className="Icon"></div>
+                            <div className="IconTitle">ShockrTools</div>
+                        </section>
+                    </div>
+                    <div className="Content">
+                        <Switch>
+                            <Route exact={true} path="/" component={ViewLandingPage} />
+                            <Route path="/base/:baseId" component={ViewBase} />
+                            <Route path="/base" component={ViewBase} />
+                            <Route path="/scan/:scanId" component={ViewScan} />
+                            <Redirect to="/" />
+                        </Switch>
+                    </div>
                 </div>
             </BrowserRouter>
         );
