adf50feca1115b34bf31a4931c5d592217c6162f
Blayne Chard <blayne@chard.com>
2020-02-06 01:48:29 +1300

    feat: allow fetching of world data

--- packages/st-backend/src/index.ts
+++ packages/st-backend/src/index.ts
@@ -10,6 +10,7 @@ import { ApiScan } from './routes/scan';
 import { ApiCall } from './api.call';
 import { ApiInstall } from './routes/install';
 import { ApiClaimPlayerStart, ApiClaimPlayerAccept } from './routes/claim';
+import { ApiWorldUpdate } from './routes/update.world';
 
 const Api = express();
 Api.use(cors({ maxAge: 24 * 60 * 60 }));
@@ -19,5 +20,6 @@ ApiCall.bind(Api, new ApiScan());
 ApiCall.bind(Api, new ApiInstall());
 ApiCall.bind(Api, new ApiClaimPlayerStart());
 ApiCall.bind(Api, new ApiClaimPlayerAccept());
+ApiCall.bind(Api, new ApiWorldUpdate());
 
 export const api = https.onRequest(Api);
