0647295a7ce9a364aa7008a0970b3ef33e0016c9
Blayne Chard <blayne@chard.com>
2020-02-16 15:57:53 +1300

    fix: allow worldIds that are higher than 411

--- packages/shared/src/validate.ts
+++ packages/shared/src/validate.ts
@@ -8,7 +8,7 @@ export function isValidWorld(worldId?: WorldId) {
         return false;
     }
     // TODO do we actually want the max world ID here?
-    if (worldId < 0 || worldId > 411) {
+    if (worldId < 0 || worldId > 1000) {
         return false;
     }
     return true;
